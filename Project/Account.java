b113edfbdd8e99d5c88b713ec9e0259b95748657
AarthyV04 <190252566+AarthyV04@users.noreply.github.com>
2025-11-16 12:49:57 +0530

    Refactor Account class with improved methods

--- Project/Account.java
+++ Project/Account.java
@@ -1,94 +1,63 @@
 import java.text.DecimalFormat;
-import java.util.*;
-public class Account {
-	Scanner input = new Scanner(System.in);
-	DecimalFormat moneyFormat = new DecimalFormat("'$'###,##0.00");
-	public int setCustomerNumber(int customerNumber) {
-		this.customerNumber = customerNumber;
-		return customerNumber;
-	}
-	public int getCustomerNumber() {
-		return customerNumber;
-	}
-	public int setPinNumber(int pinNumber) {
-		this.pinNumber = pinNumber;
-		return pinNumber;
-	}
-	public int getPinNumber() {
-		return pinNumber;
-	}
-	public double getCheckingBalance() {
-		return checkingBalance;
-	}
-	public double getSavingBalance() {
-		return savingBalance;
-	}
-	public double calcCheckingWithdraw(double amount) {
-		checkingBalance = (checkingBalance - amount);
-			return checkingBalance;
-		}
-	public double calcSavingWithdraw(double amount) {
-		savingBalance = (savingBalance - amount);
-			return savingBalance;
-		}
-	public double calcCheckingDeposit(double amount) {
-		checkingBalance = (checkingBalance + amount);
-			return checkingBalance;
-		}
-	public double calcSavingDeposit(double amount) {
-		savingBalance = (savingBalance + amount);
-			return savingBalance;
-		}
-	public void getCheckingWithdrawInput() {
-		System.out.println("Checking Account Balance: " + moneyFormat.format(checkingBalance));
-		System.out.print("Amount you want to withdraw from Checking Account: ");
-		double amount = input.nextDouble();
-		if((checkingBalance - amount)>=0) {
-			calcCheckingWithdraw(amount);
-			System.out.println("New Checking Account Balance: " + moneyFormat.format(checkingBalance));
-		}else {
-			System.out.println("Balance cannot be negative." + "\n");
-		}
-	}
-	public void getSavingWithdrawInput() {
-		System.out.println("Saving Account Balance: " + moneyFormat.format(savingBalance));
-		System.out.print("Amount you want to withdraw from Saving Account: ");
-		double amount = input.nextDouble();
-		if((savingBalance - amount)>=0) {
-			calcSavingWithdraw(amount);
-			System.out.println("New Saving Account Balance: " + savingBalance + "\n");
-		}else {
-			System.out.println("Balance cannot be negative." + "\n");
-		}
-	}
-	public void getCheckingDepositInput() {
-		System.out.println("Checking Account Balance: " + moneyFormat.format(checkingBalance));
-		System.out.print("Amount you want to withdraw from Checking Account: ");
-		double amount = input.nextDouble();
-		if((checkingBalance + amount)>=0) {
-			calcCheckingDeposit(amount);
-			System.out.println("New Checking Account Balance: " + moneyFormat.format(checkingBalance));
-		}else {
-			System.out.println("Balance cannot be negative." + "\n");
-		}
-	}
-	public void getSavingDepositInput() {
-		System.out.println("Checking Account Balance: " + moneyFormat.format(savingBalance));
-		System.out.print("Amount you want to withdraw from Checking Account: ");
-		double amount = input.nextDouble();
-		if((savingBalance + amount)>=0) {
-			calcSavingDeposit(amount);
-			System.out.println("New Checking Account Balance: " + moneyFormat.format(savingBalance));
-		}else {
-			System.out.println("Balance cannot be negative." + "\n");
-		}
-	}
-	private int customerNumber;
-	private int pinNumber;
-	private double checkingBalance = 0;
-	private double savingBalance = 0;
-	
-	
-}
 
+public class Account {
+
+    private int customerNumber;
+    private int pinNumber;
+    private double checkingBalance = 0;
+    private double savingBalance = 0;
+
+    DecimalFormat moneyFormat = new DecimalFormat("'$'###,##0.00");
+
+    // Setters & Getters
+    public int getCustomerNumber() {
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(int customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public int getPinNumber() {
+        return pinNumber;
+    }
+
+    public void setPinNumber(int pinNumber) {
+        this.pinNumber = pinNumber;
+    }
+
+    public double getCheckingBalance() {
+        return checkingBalance;
+    }
+
+    public double getSavingBalance() {
+        return savingBalance;
+    }
+
+    // Withdraw Logic
+    public boolean withdrawChecking(double amount) {
+        if (amount <= checkingBalance) {
+            checkingBalance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean withdrawSaving(double amount) {
+        if (amount <= savingBalance) {
+            savingBalance -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    // Deposit Logic
+    public void depositChecking(double amount) {
+        checkingBalance += amount;
+    }
+
+    public void depositSaving(double amount) {
+        savingBalance += amount;
+    }
+}
 
